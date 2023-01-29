dfbf6dfd53ae6b191259d4a433a0fdaefad41762
Carlos Manuel González <cmglezpdev@gmail.com>
2023-01-29 00:03:11 -0500

    feat(Root): Add fonts and configs

---
+++ tailwind.config.cjs
@@ -0,0 +1,32 @@
+/** @type {import('tailwindcss').Config} */
+
+module.exports = {
+  content: ["./src/**/*.{html,astro,jsx,js}"],
+  theme: {
+    extend: {
+      colors: {        
+        "gray-superlight": "#d8d5d5",
+        "gray-light": "#9d9d9d",
+        "gray": "#303030",
+        "gray-dark": "#212121",
+        "gray-superdark": "#19191c",
+        "green": "#40d8bb",
+      },
+      backgroundColor: {
+        "gray-light": "#c2c2c2",
+        "gray": "#55545a",
+        "gray-dark": "#2a2830",
+        "gray-black": "#1d1b24",
+        "gray-superblack": "#17151b",
+        "green-light": "#53f6c7",
+        "green": "#19d19d",
+        "pink-dark": "#f21e4e",
+      },
+      fontFamily: {
+        title: ["Rubik", "sans-serif"],
+        text: ["Roboto", "sans-serif"]        
+      },
+    },
+  },
+  plugins: [],
+}
